d5bc5b4252f363a20ffbe3648a867640d329e454
junhyeok0331 <52dhwnsgur@naver.com>
2025-05-21 19:46:37 +0900

    Add backend as normal folder

---
+++ backend/api/post/postController.js
@@ -0,0 +1,79 @@
+//PostController에서 API 함수를 만들고고
+
+const express = require('express');
+const router = express.Router();
+const postService = require('./postService');
+
+//createPost
+const createPost = async(req, res, next) => {
+    try{
+        const { title, description } = req.body;
+        console.log(`createPost - title: ${title}, description: ${description}`);
+        await postService.create(title, description); //Post 생성 부분분
+        res.status(200).json({title, description});
+    } catch (err) {
+        res.status(404);
+        next(err);
+    }
+}
+
+//getPost
+const getPost = async(req, res, next) => {
+    try{
+        const id = parseInt(req.params.id);
+        console.log(`getPost - postId: ${id}`);
+        const post = await postService.getOne(id);
+        res.status(200).json(post);
+    } catch (err){
+        res.status(404);
+        next(err);
+    }
+};
+
+//getAllPosts
+const getAllPosts = async(req, res, next) => {
+    try{
+        const posts = await postService.getAll();
+        console.log('getAllPosts');
+        res.status(200).json(posts);
+    } catch (err){
+        res.status(404);
+        next(err);
+    }
+};
+
+//deletePost
+const deletePost = async(req, res, next) => {
+    try {
+        const postId = parseInt(req.params.id);
+        console.log(`deletePost - postId: ${postId}`);
+        await postService.delete(postId);
+        res.status(200).json('삭제 완료');
+    } catch (err) {
+        res.status(404);
+        next(err);
+    }
+};
+
+//updatePost
+const updatePost = async (req, res, next) => {
+    try {
+        const postId = parseInt(req.params.id);
+        const { title, description } = req.body;
+
+        console.log(`updatePost - postId: ${postId}, title: ${title}, description: ${description}`);
+
+        await postService.update(postId, title, description);
+        res.status(200).json({ title, description });
+    } catch (err) {
+        res.status(404);
+        next(err);
+    }
+};
+
+router.get('/getOne/:id', getPost);
+router.post('/postCreate', createPost);
+router.get('/getAll', getAllPosts);
+router.delete('/delete/:id', deletePost);
+router.put('/update/:id', updatePost);
+module.exports = router;
---
+++ backend/app.js
@@ -0,0 +1,24 @@
+//port info
+const dotenv = require('dotenv');
+dotenv.config();
+const HTTP_PORT = process.env.HTTP_PORT;
+const HOST = process.env.HOST;
+
+//make express server
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+const{ sequelize } = require('./models');
+sequelize.sync();
+
+module.exports = { app };
+
+const postController = require('./api/post/postController');
+app.use('/', postController);
+
+//check server is running
+app.listen(HTTP_PORT, HOST, () => {
+    console.log(`server is on http://${HOST}:${HTTP_PORT}`);
+});
